351f10c7e7806aa8c03298bf39164d0bc8f5c355
ranjith <ranjithkumar2k1@gmail.com>
2023-07-12 09:34:39 +0000

    API Routing

---
+++ app.js
@@ -0,0 +1,13 @@
+let express = require("express");
+
+let app = express();
+
+app.get("/", (request, response) => {
+  response.send("Home Page");
+});
+
+app.get("/about", (request, response) => {
+  response.send("About Page");
+});
+
+module.exports = app;
